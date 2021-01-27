dae213b0879065e6eb5ecb45efa0bab422a043bb
Eric S. Londres <elondres@stevens.edu>
2021-01-26 22:58:26 -0500

    add more data

--- resume.lua
+++ resume.lua
@@ -23,11 +23,21 @@ local function website()
    return data.info.website
 end
 
+local function education()
+   return {
+      date = data.education.when,
+      degree = data.education.degree,
+      university = data.education.university,
+      location = data.education.city,
+      descriptions = data.education.descriptions
+   }
+end
+
 return {
    first_name = first_name,
    last_name = last_name,
    phone = phone,
    email = email,
-   website = website
-   
+   website = website,
+   education = education
 }
